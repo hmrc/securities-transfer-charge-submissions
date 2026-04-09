0ad6a5083e22781337767694539de0e3057b75df
jon-humble-hmrc <194776673+jon-humble-hmrc@users.noreply.github.com>
2026-04-09 15:38:41 +0100

    Fix deprecation warning

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,10 +1,9 @@
 import sbt.Keys.libraryDependencies
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.1.0"
-
+  private val bootstrapVersion = "10.7.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion
@@ -15,8 +14,10 @@ object AppDependencies {
     
   )
 
+  // These dependencies are attached to the dedicated `it` subproject in build.sbt,
+  // so they do not need the deprecated `IntegrationTest` configuration suffix.
   val it = Seq(
-    "com.github.tomakehurst"   % "wiremock-jre8"              % "3.0.1"                     % IntegrationTest,
-    "org.scalatestplus.play"  %% "scalatestplus-play"         % "7.0.1"                     % IntegrationTest
+    "com.github.tomakehurst"   % "wiremock-jre8"              % "3.0.1",
+    "org.scalatestplus.play"  %% "scalatestplus-play"         % "7.0.2"
   )
 }
